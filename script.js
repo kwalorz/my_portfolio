c3dee1ee5f63691fe686bcc96fdd49acae1f2436
kwalorz <kmwalorz@gmail.com>
2021-03-22 17:46:40 -0400

    update 03/22/2021

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
+//Navbar
 const navbar = document.querySelector("nav");
 
 window.onload = (e) => {
   setTimeout(() => {
-    navbar.style.transition = "all ease-in 3s";
+    navbar.style.transition = "all ease-in 2.5s";
     navbar.style.transform = "translateX(0%)";
-  }, 4000);
+  }, 0000);
 };
+//Modal Windows
+const modalAbout = document.querySelector("#about-section");
+const aboutBtn = document.querySelector("#about-btn");
+const modalContact = document.querySelector("#contact-section");
+const contactBtn = document.querySelector("#contact-btn");
+const closeBtn = document.querySelectorAll(".close");
+
+aboutBtn.addEventListener("click", () => {
+  modalAbout.style.display = "block";
+});
+
+contactBtn.addEventListener("click", () => {
+  modalContact.style.display = "block";
+});
+
+closeBtn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (e.target === modalAbout || modalContact) {
+      modalAbout.style.display = "none";
+      modalContact.style.display = "none";
+      console.log(e);
+    }
+  });
+});
+
+// window.onclick = function (e) {
+//   if (e.target === modalAbout || modalContact) {
+//     e.currentTarget.style.display = "none";
+//   }
+// };
