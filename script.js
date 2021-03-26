0ef7c08d84b39df04c2cab685f7982e6c2d971c9
kwalorz <kmwalorz@gmail.com>
2021-03-26 19:41:21 -0400

    update 03/26/2021

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Navbar
+//////////////////////Navbar///////////////////////////////////////
 const navbar = document.querySelector("nav");
 
 window.onload = (e) => {
@@ -8,8 +8,11 @@ window.onload = (e) => {
   }, 1000);
 };
 /////////////////////Modal Windows////////////////////////////////
+
 const modalAbout = document.querySelector("#about-section");
 const aboutBtn = document.querySelector("#about-btn");
+const modalResume = document.querySelector("#resume-section");
+const resumeBtn = document.querySelector("#resume-btn");
 const modalContact = document.querySelector("#contact-section");
 const contactBtn = document.querySelector("#contact-btn");
 const closeBtn = document.querySelectorAll(".close");
@@ -18,21 +21,22 @@ aboutBtn.addEventListener("click", () => {
   modalAbout.style.display = "block";
 });
 
+resumeBtn.addEventListener("click", () => {
+  modalResume.style.display = "block";
+});
+
 contactBtn.addEventListener("click", () => {
   modalContact.style.display = "block";
 });
 
 closeBtn.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    if (e.target === modalAbout || modalContact) {
-      modalAbout.style.display = "none";
-      modalContact.style.display = "none";
-      console.log(e);
-    }
+    e.target.closest(".modal").style = "none";
   });
 });
 
 window.onclick = function (e) {
   if (e.target == modalAbout) e.target.style.display = "none";
+  if (e.target == modalResume) e.target.style.display = "none";
   if (e.target == modalContact) e.target.style.display = "none";
 };
