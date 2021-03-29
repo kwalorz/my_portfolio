90720cdbfa89aa16502052227f9f95d821f4170e
kwalorz <kmwalorz@gmail.com>
2021-03-29 17:04:05 -0400

    update 03/29/2021

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ window.onload = (e) => {
 };
 /////////////////////Modal Windows////////////////////////////////
 
+const modalProjects = document.querySelector("#projects-section");
+const projectsBtn = document.querySelector("#projects-btn");
 const modalAbout = document.querySelector("#about-section");
 const aboutBtn = document.querySelector("#about-btn");
 const modalResume = document.querySelector("#resume-section");
@@ -16,6 +18,10 @@ const resumeBtn = document.querySelector("#resume-btn");
 const modalContact = document.querySelector("#contact-section");
 const contactBtn = document.querySelector("#contact-btn");
 const closeBtn = document.querySelectorAll(".close");
+
+projectsBtn.addEventListener("click", () => {
+  modalProjects.style.display = "block";
+});
 
 aboutBtn.addEventListener("click", () => {
   modalAbout.style.display = "block";
@@ -36,6 +42,7 @@ closeBtn.forEach((btn) => {
 });
 
 window.onclick = function (e) {
+  if (e.target == modalProjects) e.target.style.display = "none";
   if (e.target == modalAbout) e.target.style.display = "none";
   if (e.target == modalResume) e.target.style.display = "none";
   if (e.target == modalContact) e.target.style.display = "none";
